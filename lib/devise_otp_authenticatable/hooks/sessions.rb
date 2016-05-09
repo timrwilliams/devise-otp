f9b5e2f3863637b6b26c47b7e1742adb9b82b123
Tim Williams <timwemail@gmail.com>
2016-05-09 10:01:38 +0100

    restore stored_location for non-otp logins

--- lib/devise_otp_authenticatable/hooks/sessions.rb
+++ lib/devise_otp_authenticatable/hooks/sessions.rb
@@ -28,7 +28,7 @@ module DeviseOtpAuthenticatable::Hooks
         sign_in(resource_name, resource)
         respond_with resource, :location => otp_token_path_for(resource)
       else
-
+        store_location_for(resource, devise_stored_location) # restore the stored location
         set_flash_message(:notice, :signed_in) if is_navigational_format?
         sign_in(resource_name, resource)
         respond_with resource, :location => after_sign_in_path_for(resource)
